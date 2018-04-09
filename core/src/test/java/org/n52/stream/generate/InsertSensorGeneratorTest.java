533cd456fd0a2584ebc44f295e7841f897d4c5db
Carsten Hollmann <c.hollmann@52north.org>
2018-04-09 12:58:23 +0200

    enhance tests

--- core/src/test/java/org/n52/stream/generate/InsertSensorGeneratorTest.java
+++ core/src/test/java/org/n52/stream/generate/InsertSensorGeneratorTest.java
@@ -8,11 +8,17 @@ import org.apache.xmlbeans.XmlException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.n52.shetland.ogc.gml.AbstractFeature;
 import org.n52.shetland.ogc.om.OmConstants;
 import org.n52.shetland.ogc.om.features.SfConstants;
+import org.n52.shetland.ogc.om.features.samplingFeatures.AbstractSamplingFeature;
+import org.n52.shetland.ogc.sensorML.AbstractProcess;
+import org.n52.shetland.ogc.sensorML.AbstractSensorML;
 import org.n52.shetland.ogc.sensorML.SensorML20Constants;
+import org.n52.shetland.ogc.sensorML.v20.AbstractProcessV20;
 import org.n52.shetland.ogc.sensorML.v20.AggregateProcess;
 import org.n52.shetland.ogc.sensorML.v20.PhysicalSystem;
+import org.n52.shetland.ogc.sensorML.v20.SmlFeatureOfInterest;
 import org.n52.shetland.ogc.sos.SosInsertionMetadata;
 import org.n52.shetland.ogc.sos.request.InsertSensorRequest;
 import org.n52.stream.AbstractCodingTest;
@@ -61,6 +67,35 @@ public class InsertSensorGeneratorTest extends AbstractCodingTest {
         Assert.isTrue(request.getProcedureDescriptionFormat().equals(SensorML20Constants.NS_SML_20), "ProcedureDescriptionFormat should be http://www.opengis.net/sensorml/2.0");
         checkObservedProperties(request);
         checkMetadata(request);
+    }
+    
+    @Test
+    public void generateHrefFeature()
+            throws DecodingException,
+            IOException,
+            XmlException {
+        Assert.isTrue(decode != null, "Should not be null");
+        Assert.isTrue(decode instanceof AggregateProcess, "Should be instance of AggregateProcess");
+        AggregateProcess aggregateProcess = (AggregateProcess) decode;
+        Assert.isTrue(aggregateProcess.isSetComponents(), "Should have Components");
+        Assert.isTrue(aggregateProcess.getComponents().size() == 2, "Components size should be 2");
+        Assert.isTrue(aggregateProcess.getComponents().get(1).getProcess() instanceof PhysicalSystem,
+                "Component 2 should be a PhysicalSystem");
+        AbstractSensorML process = aggregateProcess.getComponents().get(1).getProcess();
+        if (process instanceof AbstractProcessV20) {
+            AbstractProcessV20 p = (AbstractProcessV20) process;
+            for (AbstractFeature f : p.getSmlFeatureOfInterest().getFeaturesOfInterestMap().values()) {
+                if (f instanceof AbstractSamplingFeature) {
+                    ((AbstractSamplingFeature) f).setGeometry(null);
+                }
+            }
+            InsertSensorRequest request = generator.generate(p);
+            Assert.isTrue(request.isSetProcedureDescription(), "Should have ProcedureDescription");
+            Assert.isTrue(request.isSetProcedureDescriptionFormat(), "Should have ProcedureDescriptionFormat");
+            Assert.isTrue(request.getProcedureDescriptionFormat().equals(SensorML20Constants.NS_SML_20), "ProcedureDescriptionFormat should be http://www.opengis.net/sensorml/2.0");
+            checkObservedProperties(request);
+            checkMetadata(request);
+        }
     }
 
     private void checkObservedProperties(InsertSensorRequest request) {
