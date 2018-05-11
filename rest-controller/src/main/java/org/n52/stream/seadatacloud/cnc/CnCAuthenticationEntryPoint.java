12f14a40bf7bf2e60dc11b75347ffab4a4106156
Carsten Hollmann <c.hollmann@52north.org>
2018-05-11 15:12:58 +0200

    fix security, GET is not secured because it is also used internally for SensorML

---
+++ rest-controller/src/main/java/org/n52/stream/seadatacloud/cnc/CnCAuthenticationEntryPoint.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018-2018 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+package org.n52.stream.seadatacloud.cnc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CnCAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+        @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+      throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+        @Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("C&C Service");
+        super.afterPropertiesSet();
+    }
+
+}
