3b2c5de763c4d3c8899c185b09d5c6e8d9c835c5
MojioMS <maurinradtke@gmail.com>
2018-09-20 14:39:20 +0200

    added CSV-FileSplitter

---
+++ csv-filesplitter/src/main/java/org/n52/stream/seadatacloud/processors/AppConfiguration.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2018-2018 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+package org.n52.stream.seadatacloud.processors;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * @author <a href="mailto:m.radtke@52north.org">Maurin Radtke</a>
+ *
+ */
+@Validated
+@ConfigurationProperties("org.n52.stream")
+public class AppConfiguration {
+
+    /**
+     * delimiter to split one multi lines message into multiple single line messages.
+     */
+    private String delimiter = ";";
+    
+    /**
+     * url to mark the file_name by url
+     */
+    private String url = "";
+    
+    /**
+     * number of maximum messages to forward to the output channel per poll.
+     */
+    private int maxMessages = Integer.MAX_VALUE;
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getMaxMessages() {
+        return maxMessages;
+    }
+
+    public void setMaxMessages(int maxMessages) {
+        this.maxMessages = maxMessages;
+    }
+    
+}
